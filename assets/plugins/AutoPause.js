492b005428fdbf9d85a81b698fc4ebd0c097b8e3
Esteban Alvarado <estalvgs1999@estudiantec.cr>
2020-12-25 19:33:51 -0600

    Se agrega funcionalidad de Pausar video según VisiblityChange

--- assets/plugins/AutoPause.js
+++ assets/plugins/AutoPause.js
@@ -2,20 +2,30 @@ class AutoPause {
 
   constructor(){
     this.threshold = 0.25;
-    this.hanldeIntersection = this.hanldeIntersection.bind(this);
+    this.handleIntersection = this.handleIntersection.bind(this);
+    this.handleVisibilityChange = this.handleVisibilityChange.bind(this);
   }
   
   run(player) {
     this.player = player;
-    const observer = new IntersectionObserver(this.hanldeIntersection, 
-      {
-        threshold : this.threshold,
-      });
-
+    
+    const observer = new IntersectionObserver(this.handleIntersection, {threshold : this.threshold});
     observer.observe(this.player.media);
+
+    document.addEventListener("visibilitychange",this.handleVisibilityChange)
   }
 
-  hanldeIntersection(entries) {
+  handleVisibilityChange(){
+    const isVisible = document.visibilityState === "visible";
+
+    if (isVisible){
+      this.player.play();
+    }else{
+      this.player.pause();
+    }
+  }
+
+  handleIntersection(entries) {
     const entry = entries[0];
     const isVisible = entry.intersectionRatio >= this.threshold;
 
